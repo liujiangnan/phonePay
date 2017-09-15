b940e4041fc1a9a373af5a25dd5e1f91d6120984
liujiangnan <695541723@qq.com>
2017-09-15 22:52:27 +0800

    新建项目

---
+++ engine/phone-pay/src/service.js
@@ -0,0 +1,24 @@
+
+
+function service(net){
+
+  this.list = async function(ctx,param){  
+    return ctx.render("engine/phone-pay/web/list.ejs",{});
+  }
+
+  this.search = function(data){
+    let items = [];
+    for(var i=0;i<15;i++){
+      items.push({
+        date:new Date().pattern("yyyy-MM-dd"),
+        username:'test',
+        chargeType:'充金币',
+        money:'100元',
+        payType:'微信'
+      });
+    } 
+    net.data.items = items; 
+  }
+
+}
+module.exports = service;
